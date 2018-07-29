207f64062dc9d9be1e94124bd170595388fd1bfa
Dhruv Joshi <joshidhruv997@gmail.com>
2018-07-29 14:09:48 +0530

    It's about time. Overloaded basic operators. And yes. We can perform dot and cross products on vectors now.

--- include/Vector3.hpp
+++ include/Vector3.hpp
@@ -13,9 +13,32 @@ namespace gs {
             // Destructor
             virtual ~Vector3() {}
 
-            // Copy Constructors
+            // Copy constructors
             Vector3(const Vector3& v);
             Vector3& operator=(const Vector3& v);
+
+            // Operator overloads
+            void operator+=(const Vector3& v);
+            void operator-=(const Vector3& v);
+            void operator/=(const float s);
+            Vector3 operator+(const Vector3& v);
+            Vector3 operator-(const Vector3& v);
+            Vector3 operator/(const float s) const;
+
+            // Scalar product
+            void operator*=(const float s);
+            Vector3 operator*(const float s) const;
+
+            // Dot product
+            float operator*(const Vector3& v) const;
+            float dot(const Vector3& v) const;
+
+            // Cross product
+            Vector3 cross(const Vector3& v) const;
+            void operator%=(const Vector3& v);
+            Vector3 operator%(const Vector3& v) const;
+
+            void Show();
             
             // Vector components
             float x;
--- src/Vector3.cpp
+++ src/Vector3.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "Vector3.hpp"
 
 namespace gs {
@@ -10,6 +11,82 @@ namespace gs {
         z = v.z;
 
         return *this;
+    }
+
+    void Vector3::operator+=(const Vector3& v) {
+        x += v.x;
+        y += v.y;
+        z += v.z;
+    }
+
+    Vector3 Vector3::operator+(const Vector3& v) {
+        return Vector3(x + v.x,
+                       y + v.y,
+                       z + v.z);
+    }
+
+    void Vector3::operator-=(const Vector3& v) {
+        x -= v.x;
+        y -= v.y;
+        z -= v.z;
+    }
+
+    Vector3 Vector3::operator-(const Vector3& v) {
+        return Vector3(x - v.x,
+                       y - v.y,
+                       z - v.z);
+    }
+
+    void Vector3::operator*=(const float s) {
+        x *= s;
+        y *= s;
+        z *= s;
+    }
+
+    Vector3 Vector3::operator*(const float s) const {
+        return Vector3(x * s,
+                       y * s,
+                       z * s);
+    }
+
+    void Vector3::operator/=(const float s) {
+        x /= s;
+        y /= s;
+        z /= s;
+    }
+
+    Vector3 Vector3::operator/(const float s) const {
+        return Vector3(x / s,
+                       y / s,
+                       z / s);
+    }
+
+    float Vector3::operator*(const Vector3& v) const {
+        return x*v.x + y*v.y + z*v.z;
+    }
+
+    float Vector3::dot(const Vector3& v) const {
+        return x*v.x + y*v.y + z*v.z;
+    }
+
+    Vector3 Vector3::cross(const Vector3& v) const {
+        return Vector3(y * v.z - z * v.y,
+                       z * v.x - x * v.z,
+                       x * v.y - y * v.x);
+    }
+
+    void Vector3::operator%=(const Vector3& v) {
+        *this = cross(v);
+    }
+
+    Vector3 Vector3::operator%(const Vector3& v) const {
+        return Vector3(y * v.z - z * v.y,
+                       z * v.x - x * v.z,
+                       x * v.y - y * v.x);
+    }
+
+    void Vector3::Show() {
+        std::cout << "(" << x << ", " << y << ", " << z << ")" << std::endl;
     }
 
 }
